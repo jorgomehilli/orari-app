7caab35ccd1b027bc40cfef9858c45196b88a6ce
Jorgo Mehilli <jorgo.mehilli@lhind.dlh.de>
2021-02-18 23:20:41 +0100

    Menu + first table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClassComponent } from './class/class.component';
+import { MenuComponent } from './menu/menu.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+  { path: 'class', component: ClassComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/class/class.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-class',
+  templateUrl: './class.component.html',
+  styleUrls: ['./class.component.css']
+})
+export class ClassComponent implements OnInit {
+
+currentDate = new Date;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
